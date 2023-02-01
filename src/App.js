ba1367942a34797b01080e8b30184e5541111fae
drameBoubacar <boubacardrame9@gmail.com>
2023-02-01 19:48:02 +0100

    change architecture

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
-import Header from './components/Header';
-import Input from './components/Input';
+import React, {useState} from 'react'
+import List from './components/List';
+
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todos, setTodo] = useState([]);
 
+  const handleInputValue = (e) => {
+    setInputValue(e.target.value);
+  }
+
+  const handleClick = (inputValue) => {
+    setTodo([...todos, inputValue])
+  }
   return (
-    <div>
-      <Header/>
-      <Input/>
+    <div className="App">
+      <h1>TodoList</h1>
+      <input type="text" onChange={handleInputValue}/>
+      <button onClick={() => handleClick(inputValue)}>Create</button>
+      <List/>
     </div>
   );
 }
